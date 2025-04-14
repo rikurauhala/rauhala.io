d3c506616fe8f58f9480c5744ed6e81b6837f9e5
Riku Rauhala <riku@rauhala.io>
2025-04-14 20:52:21 +0300

    Update profile picture and add gradient border

--- src/components/InfoSection/ProfilePicture.tsx
+++ src/components/InfoSection/ProfilePicture.tsx
@@ -1,14 +1,23 @@
-import { Avatar } from "@radix-ui/themes";
+import { Avatar, Box } from "@radix-ui/themes";
+
+import { author } from "~/content/info";
 
 export const ProfilePicture = () => {
   return (
-    <Avatar
-      alt="Profile picture"
-      fallback="R"
-      radius="full"
-      size="9"
-      src="/img/profile.png"
-      title="Profile picture"
-    />
+    <Box
+      style={{
+        background: "linear-gradient(45deg, var(--crimson-11), var(--blue-11))",
+        borderRadius: "50%",
+        padding: "2px",
+      }}
+    >
+      <Avatar
+        alt={`Profile picture of ${author.name}`}
+        fallback="R"
+        radius="full"
+        size="9"
+        src="/img/riku-rauhala.jpg"
+      />
+    </Box>
   );
 };
