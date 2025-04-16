b3d3a079346a5491c4b409547f9d70557da415ae
Riku Rauhala <riku@rauhala.io>
2025-04-16 13:08:32 +0300

    Remove email contact button

--- src/components/InfoSection/index.tsx
+++ src/components/InfoSection/index.tsx
@@ -23,8 +23,6 @@ export const InfoSection = () => {
         <ContactButton variant="gitHub" />
         <Separator orientation="vertical" />
         <ContactButton variant="linkedIn" />
-        <Separator orientation="vertical" />
-        <ContactButton variant="email" />
       </Flex>
     </Flex>
   );
