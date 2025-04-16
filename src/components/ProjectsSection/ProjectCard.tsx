2263e17945f1e81488298a0c1ff95e840a44bda2
Riku Rauhala <riku@rauhala.io>
2025-04-16 09:05:50 +0300

    Turn the project list into a grid

--- src/components/ProjectsSection/ProjectCard.tsx
+++ src/components/ProjectsSection/ProjectCard.tsx
@@ -1,16 +1,37 @@
-import { Card, Heading, Text } from "@radix-ui/themes";
+import {
+  AspectRatio,
+  Box,
+  Card,
+  Flex,
+  Heading,
+  Inset,
+  Text,
+} from "@radix-ui/themes";
 
 import { Project } from "~/types";
 
 export const ProjectCard = ({ project }: { project: Project }) => {
   return (
     <Card>
-      <Heading as="h3" size="4">
-        {project.name}
-      </Heading>
-      <Text as="p" color="gray">
-        {project.description}
-      </Text>
+      <Inset clip="padding-box" side="top" pb="current">
+        <AspectRatio ratio={16 / 9}>
+          <Box
+            style={{
+              background:
+                "linear-gradient(45deg, var(--crimson-10), var(--blue-10))",
+              height: "100%",
+            }}
+          />
+        </AspectRatio>
+      </Inset>
+      <Flex direction="column" gap="2">
+        <Heading as="h3" size="4">
+          {project.name}
+        </Heading>
+        <Text as="p" color="gray">
+          {project.description}
+        </Text>
+      </Flex>
     </Card>
   );
 };
--- src/components/ProjectsSection/index.tsx
+++ src/components/ProjectsSection/index.tsx
@@ -1,4 +1,4 @@
-import { Flex } from "@radix-ui/themes";
+import { Flex, Grid } from "@radix-ui/themes";
 
 import { TextContainer } from "~/components/TextContainer";
 import { projects, projectsSectionContent } from "~/content/projects";
@@ -8,9 +8,11 @@ export const ProjectsSection = () => {
   return (
     <Flex direction="column" gap="4">
       <TextContainer content={projectsSectionContent} />
-      {projects.map((project) => (
-        <ProjectCard key={project.key} project={project} />
-      ))}
+      <Grid columns={{ initial: "1", sm: "2" }} gap="4">
+        {projects.map((project) => (
+          <ProjectCard key={project.key} project={project} />
+        ))}
+      </Grid>
     </Flex>
   );
 };
