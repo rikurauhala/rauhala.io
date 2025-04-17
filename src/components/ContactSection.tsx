fce08fc9943cb13a65e8c12f5ec4baca3357356b
Riku Rauhala <riku@rauhala.io>
2025-04-17 10:35:06 +0300

    Improve contact form

--- src/components/ContactSection.tsx
+++
@@ -1,6 +0,0 @@
-import { TextContainer } from "~/components/TextContainer";
-import { contactSectionContent } from "~/content/contact";
-
-export const ContactSection = () => {
-  return <TextContainer content={contactSectionContent} />;
-};
--- src/components/ContactSection/index.tsx
+++ src/components/ContactSection/index.tsx
@@ -1,5 +1,5 @@
 import { EnvelopeClosedIcon, PersonIcon } from "@radix-ui/react-icons";
-import { Button, Flex, TextArea, TextField } from "@radix-ui/themes";
+import { Box, Button, Flex, Text, TextArea, TextField } from "@radix-ui/themes";
 
 import { TextContainer } from "~/components/TextContainer";
 import { contactSectionContent } from "~/content/contact";
@@ -8,18 +8,36 @@ export const ContactSection = () => {
   return (
     <Flex direction="column" gap="4">
       <TextContainer content={contactSectionContent} />
-      <Flex direction="column" gap="4">
-        <TextField.Root placeholder="Your name">
-          <TextField.Slot>
-            <PersonIcon />
-          </TextField.Slot>
-        </TextField.Root>
-        <TextField.Root placeholder="Your email address" type="email">
-          <TextField.Slot>
-            <EnvelopeClosedIcon />
-          </TextField.Slot>
-        </TextField.Root>
-        <TextArea placeholder="Your message" resize="vertical" />
+      <Flex direction="column" gap="3">
+        <Box>
+          <Text color="gray" size="2">
+            Your name
+          </Text>
+          <TextField.Root placeholder="Type your name here">
+            <TextField.Slot>
+              <PersonIcon />
+            </TextField.Slot>
+          </TextField.Root>
+        </Box>
+
+        <Box>
+          <Text color="gray" size="2">
+            Your email
+          </Text>
+          <TextField.Root placeholder="Your email address" type="email">
+            <TextField.Slot>
+              <EnvelopeClosedIcon />
+            </TextField.Slot>
+          </TextField.Root>
+        </Box>
+
+        <Box>
+          <Text color="gray" size="2">
+            Your message
+          </Text>
+          <TextArea placeholder="Your message" resize="vertical" />
+        </Box>
+
         <Flex direction="row" gap="3">
           <Button style={{ flex: 1 }} variant="outline">
             Clear form
