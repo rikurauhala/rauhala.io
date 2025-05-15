c5cb86317e3025487b102a4d85178c5a9b170c7c
Riku Rauhala <riku@rauhala.io>
2025-05-15 13:01:57 +0300

    Remove unused gradients

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -1,19 +1,5 @@
-const level = 8 as const;
-
 const getColorVariable = (colorName: string, level: number) => {
   return `var(--${colorName}-${level})`;
-};
-
-export const colors = {
-  blue: getColorVariable("blue", level),
-  indigo: getColorVariable("indigo", level),
-} as const;
-
-const gradients = [[colors.indigo, colors.blue]] as const;
-
-export const getBackgroundGradient = (index: number): string => {
-  const [color1, color2] = gradients[index % gradients.length];
-  return `linear-gradient(90deg, ${color1}, ${color2})`;
 };
 
 export const getBorderGradient = () => {
