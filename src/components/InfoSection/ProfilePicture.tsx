97aec7da1a7baef737f91e54ffd51d0bc685e3a2
Riku Rauhala <riku@rauhala.io>
2025-05-16 14:34:30 +0300

    Use next/image for the profile picture

--- src/components/InfoSection/ProfilePicture.tsx
+++ src/components/InfoSection/ProfilePicture.tsx
@@ -1,23 +1,31 @@
-import { Avatar, Box } from "@radix-ui/themes";
+import { Box } from "@radix-ui/themes";
+import Image from "next/image";
 
 import { author } from "~/content/info";
 import { getBorderGradient } from "~/utils/color";
 
 export const ProfilePicture = () => {
+  const size = 160;
+
   return (
     <Box
       style={{
         background: getBorderGradient(),
         borderRadius: "50%",
+        display: "flex",
+        justifyContent: "center",
         padding: "2px",
       }}
     >
-      <Avatar
+      <Image
         alt={`Profile picture of ${author.name}`}
-        fallback="R"
-        radius="full"
-        size="9"
+        height={size}
+        width={size}
+        priority
         src="/img/riku-rauhala.webp"
+        style={{
+          borderRadius: "50%",
+        }}
       />
     </Box>
   );
