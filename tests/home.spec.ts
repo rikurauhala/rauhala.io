cd254f94b4b5fe09820abbbaa784985525eab6bc
Riku Rauhala <riku@rauhala.io>
2025-05-19 14:30:35 +0300

    Begin testing

---
+++ tests/home.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("Home page", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/");
+  });
+
+  test("has title", async ({ page }) => {
+    await expect(page).toHaveTitle("Riku Rauhala");
+  });
+});
