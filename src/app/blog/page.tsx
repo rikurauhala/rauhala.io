99029786aad81c22b10ca5a3e71492e4581b838b
Riku Rauhala <riku@rauhala.io>
2025-05-19 10:32:02 +0300

    Implement placeholder blog page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,24 @@
+import { Container, Flex, Heading, Section, Text } from "@radix-ui/themes";
+import { Metadata } from "next";
+
+import { getTitle } from "~/utils/meta";
+
+export const metadata: Metadata = {
+  title: getTitle("Blog"),
+  description: "Blog about software engineering, web development, and more",
+};
+
+export default function BlogPage() {
+  return (
+    <Section py="6">
+      <Container px="5" size="3">
+        <Flex direction="column" gap="4">
+          <Heading as="h1" size="6">
+            Blog
+          </Heading>
+          <Text color="gray">Coming soon!</Text>
+        </Flex>
+      </Container>
+    </Section>
+  );
+}
