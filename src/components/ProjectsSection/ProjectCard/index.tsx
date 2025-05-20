400ed66b3d2a500e36fbd54e7258b09f9a9a5ed8
Riku Rauhala <riku@rauhala.io>
2025-05-20 14:30:35 +0300

    Remove buttons from project cards

--- src/components/ProjectsSection/ProjectCard/index.tsx
+++ src/components/ProjectsSection/ProjectCard/index.tsx
@@ -1,9 +1,8 @@
-import { Box, Card, Flex, Heading, Inset, Text } from "@radix-ui/themes";
+import { Card, Flex, Heading, Inset, Text } from "@radix-ui/themes";
+import Link from "next/link";
 
 import { Project } from "~/types";
-import { ReadMoreButton } from "./ReadMoreButton";
 import { ProjectImage } from "./ProjectImage";
-import { ViewSourceButton } from "./ViewSourceButton";
 
 export const ProjectCard = ({ project }: { project: Project }) => {
   return (
@@ -11,28 +10,13 @@ export const ProjectCard = ({ project }: { project: Project }) => {
       <Inset clip="padding-box" pb="current" side="top">
         <ProjectImage image={project.image} projectName={project.name} />
       </Inset>
-      <Flex direction="column" gap="4" style={{ flex: 1 }}>
-        <Flex direction="column" flexGrow="1" gap="2">
-          <Heading as="h3" size="4">
-            {project.name}
-          </Heading>
-          <Text as="p" color="gray">
-            {project.description}
-          </Text>
-        </Flex>
-        <Inset side="bottom">
-          <Box
-            p="3"
-            style={{
-              backgroundColor: "var(--gray-4)",
-            }}
-          >
-            <Flex direction="row" gap="3">
-              <ViewSourceButton repository={project.repository} />
-              <ReadMoreButton id={project.id} />
-            </Flex>
-          </Box>
-        </Inset>
+      <Flex direction="column" gap="2" style={{ flex: 1 }}>
+        <Heading as="h3" size="4">
+          <Link href={`/projects/${project.id}`}>{project.name}</Link>
+        </Heading>
+        <Text as="p" color="gray">
+          {project.description}
+        </Text>
       </Flex>
     </Card>
   );
