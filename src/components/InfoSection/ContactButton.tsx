a7987ebd2fb9954f54363e2cd9d7ef1b08b8fe96
Riku Rauhala <riku@rauhala.io>
2025-05-20 14:05:04 +0300

    Remove email variant from ContactButton

--- src/components/InfoSection/ContactButton.tsx
+++ src/components/InfoSection/ContactButton.tsx
@@ -1,24 +1,15 @@
-import {
-  EnvelopeClosedIcon,
-  GitHubLogoIcon,
-  LinkedInLogoIcon,
-} from "@radix-ui/react-icons";
+import { GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
 import { AccessibleIcon, Button } from "@radix-ui/themes";
 
 import { contact } from "~/content/info";
 
-type Variant = "email" | "gitHub" | "linkedIn";
+type Variant = "gitHub" | "linkedIn";
 
 export const ContactButton = ({ variant }: { variant: Variant }) => {
   const variants: Record<
     Variant,
     { href: string; icon: React.ReactNode; label: string }
   > = {
-    email: {
-      href: `mailto:${contact.email}`,
-      icon: <EnvelopeClosedIcon />,
-      label: "Email",
-    },
     gitHub: {
       href: `https://github.com/${contact.gitHub}`,
       icon: <GitHubLogoIcon />,
