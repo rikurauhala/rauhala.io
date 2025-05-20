27c36ad7bf8ce85b7c9af4d6b835d1090df7fbd2
Riku Rauhala <riku@rauhala.io>
2025-05-20 14:00:14 +0300

    Remove email address

--- src/content/info.ts
+++ src/content/info.ts
@@ -1,23 +1,3 @@
-import he from "he";
-
-const letters = [
-  "&#x72;",
-  "&#x69;",
-  "&#x6B;",
-  "&#x75;",
-  "&#x40;",
-  "&#x72;",
-  "&#x61;",
-  "&#x75;",
-  "&#x68;",
-  "&#x61;",
-  "&#x6C;",
-  "&#x61;",
-  "&#x2E;",
-  "&#x69;",
-  "&#x6F;",
-] as const;
-
 export const author = {
   name: "Riku Rauhala",
   profession: "Software Developer",
@@ -27,5 +7,4 @@ export const author = {
 export const contact = {
   gitHub: "rikurauhala",
   linkedIn: "rikurauhala",
-  email: he.decode(letters.join("")),
 } as const;
