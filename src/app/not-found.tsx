37a2bd6d6e671042f6cdf193bcdf3405da9ca106
Riku Rauhala <riku@rauhala.io>
2025-05-21 10:14:27 +0300

    Simplify not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,7 +8,6 @@ import {
   Text,
 } from "@radix-ui/themes";
 import { Metadata } from "next";
-import Image from "next/image";
 
 import { getTitle } from "~/utils/meta";
 
@@ -22,18 +21,12 @@ export default function NotFound() {
     <Section py="8">
       <Container px="5" size="3">
         <Flex direction="column" gap="4">
-          <Image
-            alt={`Illustration of a sad puppy looking at a page that has been eaten`}
-            src={`/img/undraw/page-eaten.svg`}
-            height={750 / 3}
-            width={645 / 3}
-          />
           <Heading as="h1" size="6">
             Not found
           </Heading>
           <Text color="gray">This page does not exist</Text>
           <Box>
-            <Link href="/">Go back to the front page</Link>
+            <Link href="/">Go back to the home page</Link>
           </Box>
         </Flex>
       </Container>
