910553d062b90b4aba9764b7d80e7e6ffeaf38f9
Riku Rauhala <riku@rauhala.io>
2025-05-27 20:40:48 +0300

    Add support for Markdown tables

---
+++ src/components/MarkdownContainer/Table.tsx
@@ -0,0 +1,43 @@
+import { Table as Table } from "@radix-ui/themes";
+
+export const TableBody = ({ children }: { children?: React.ReactNode }) => {
+  return <Table.Body>{children}</Table.Body>;
+};
+
+export const TableCell = ({
+  children,
+  style,
+}: {
+  children?: React.ReactNode;
+  style?: React.CSSProperties;
+}) => {
+  return <Table.Cell style={style}>{children}</Table.Cell>;
+};
+
+export const TableHeader = ({ children }: { children?: React.ReactNode }) => {
+  return <Table.Header>{children}</Table.Header>;
+};
+
+export const TableHeaderCell = ({
+  children,
+  style,
+}: {
+  children?: React.ReactNode;
+  style?: React.CSSProperties;
+}) => {
+  return (
+    <Table.ColumnHeaderCell style={style}>{children}</Table.ColumnHeaderCell>
+  );
+};
+
+export const TableRoot = ({ children }: { children?: React.ReactNode }) => {
+  return (
+    <Table.Root className="common" layout="auto" variant="surface">
+      {children}
+    </Table.Root>
+  );
+};
+
+export const TableRow = ({ children }: { children?: React.ReactNode }) => {
+  return <Table.Row>{children}</Table.Row>;
+};
--- src/components/MarkdownContainer/index.tsx
+++ src/components/MarkdownContainer/index.tsx
@@ -1,10 +1,19 @@
 import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 import { Heading1 } from "./Heading1";
 import { Heading2 } from "./Heading2";
 import { Image } from "./Image";
 import { Link } from "./Link";
 import { Paragraph } from "./Paragraph";
+import {
+  TableBody,
+  TableCell,
+  TableHeader,
+  TableHeaderCell,
+  TableRoot,
+  TableRow,
+} from "./Table";
 import "./style.css";
 
 export const MarkdownContainer = ({ content }: { content: string }) => {
@@ -16,7 +25,14 @@ export const MarkdownContainer = ({ content }: { content: string }) => {
         h2: Heading2,
         img: Image,
         p: Paragraph,
+        table: TableRoot,
+        tbody: TableBody,
+        td: TableCell,
+        th: TableHeaderCell,
+        thead: TableHeader,
+        tr: TableRow,
       }}
+      remarkPlugins={[remarkGfm]}
     >
       {content}
     </ReactMarkdown>
