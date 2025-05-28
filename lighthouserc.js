9745f192ab73f1076423c7ff7d665aebd41052a6
Riku Rauhala <riku@rauhala.io>
2025-05-28 11:09:26 +0300

    Update lighthouserc

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,5 +1,12 @@
 module.exports = {
   ci: {
+    collect: {
+      staticDistDir: "./out",
+      url: [
+        "http://localhost:3000/",
+        "http://localhost:3000/projects/portfolio",
+      ],
+    },
     upload: {
       target: "temporary-public-storage",
     },
