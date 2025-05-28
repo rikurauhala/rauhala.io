a199a52cb8917e2dd018209cd89cee49c304030b
Riku Rauhala <riku@rauhala.io>
2025-05-28 21:34:53 +0300

    Fix sourceUrl

--- src/content/footer.ts
+++ src/content/footer.ts
@@ -1,6 +1,6 @@
 const copyright = "© 2025 Riku Rauhala";
 const version = `Version ${process.env.npm_package_version}`;
-const sourceUrl = `https://github.com/rikurauhala/rauhala-io`;
+const sourceUrl = `https://github.com/rikurauhala/rauhala.io`;
 const licenseUrl = `${sourceUrl}/blob/main/LICENSE.md`;
 const creditsUrl = `${sourceUrl}/blob/main/docs/credits.md`;
 const changelogUrl = `${sourceUrl}/releases`;
