11528fe2e581131e6a7ab7c1550bc8368b2108d2
Riku Rauhala <riku@rauhala.io>
2025-05-30 14:48:20 +0300

    Make the table tab focusable

--- src/components/MarkdownContainer/Table.tsx
+++ src/components/MarkdownContainer/Table.tsx
@@ -1,7 +1,7 @@
 import { Table as Table } from "@radix-ui/themes";
 
 export const TableBody = ({ children }: { children?: React.ReactNode }) => {
-  return <Table.Body>{children}</Table.Body>;
+  return <Table.Body tabIndex={0}>{children}</Table.Body>;
 };
 
 export const TableCell = ({
