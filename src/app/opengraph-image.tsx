573a8b03e1af2e0566ada49204116bd00b5097b8
Riku Rauhala <riku@rauhala.io>
2025-06-03 21:49:46 +0300

    Generate OG image

---
+++ src/app/opengraph-image.tsx
@@ -0,0 +1,30 @@
+import { ImageResponse } from "next/og";
+
+import { author, description } from "~/content/info";
+
+export const contentType = "image/png";
+export const dynamic = "force-static";
+export const size = { height: 630, width: 1200 };
+
+export default async function Image() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          alignItems: "center",
+          backgroundImage: "linear-gradient(to right, #11131F, #191114)",
+          color: "#fff",
+          display: "flex",
+          flexDirection: "column",
+          gap: 16,
+          height: "100%",
+          justifyContent: "center",
+          width: "100%",
+        }}
+      >
+        <div style={{ fontSize: 64 }}>{author.name}</div>
+        <div style={{ color: "gray", fontSize: 32 }}>{description}</div>
+      </div>
+    )
+  );
+}
