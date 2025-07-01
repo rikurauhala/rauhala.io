ac36fe7ea9c809862ea88af15eeb6199905196d2
Riku Rauhala <riku@rauhala.io>
2025-07-01 21:47:28 +0300

    Extract date util

--- src/components/LastModified.tsx
+++ src/components/LastModified.tsx
@@ -1,11 +1,9 @@
 import { Text } from "@radix-ui/themes";
 
+import { formatDate } from "~/utils/date";
+
 export const LastModified = ({ date }: { date: string }) => {
-  const formattedDate = new Date(date).toLocaleDateString("en-GB", {
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-  });
+  const formattedDate = formatDate(date);
 
   return (
     <Text color="gray" size="2">
