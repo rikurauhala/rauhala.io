a2b34ce89ff3c24c522fd35c74e0fcb9758a9a53
Riku Rauhala <riku@rauhala.io>
2026-06-11 19:54:09 +0300

    Update dependency @biomejs/biome to v2.4.16

--- src/components/TextContainer.tsx
+++ src/components/TextContainer.tsx
@@ -4,6 +4,7 @@ export const TextContainer = ({ content }: { content: string }) => {
   return (
     <Flex direction="column" gap="3">
       {content.split("\n\n").map((line, index) => (
+        // biome-ignore lint/suspicious/noArrayIndexKey: Reasonable to use here.
         <Text key={index} as="p">
           {line}
         </Text>
